e6752a8d7749832d1159e16158a8324a3d128b29
Stellakim1230 <stellakim1230@gmail.com>
2019-12-15 17:04:36 +0900

    할일 추가. (TODO: 추가시 input 초기화) 할일체크박스 선택. (TODO: li 처음 요소만 반응함) 할일 삭제 (TODO: li 처음 요소만 반응함)

---
+++ mission001/kimjieun/js/app.js
@@ -0,0 +1,33 @@
+// 초기에는 카운트 나오지만, 할일 추가후 카운트도 변경되어야 한다.
+const $liCount = document.querySelector('#todo-list').childElementCount
+document.querySelector('.todo-count').innerHTML = $liCount
+
+document.querySelector('#new-todo-title').addEventListener('keyup', (e) => {
+  const $todoList = document.querySelector('#todo-list')
+  const $li = document.createElement('li')
+  const htmlTemplate = `
+    <div class="view">
+      <input class="toggle" type="checkbox">
+      <label class="label">${e.target.value}</label>
+      <button class="destroy"></button>
+    </div>
+    <input class="edit" value="${e.target.value}">
+  `
+
+  if (e.keyCode === 13) {
+    $li.innerHTML = htmlTemplate
+    $todoList.appendChild($li)
+  }
+})
+
+// data-index
+document.querySelector('.destroy').addEventListener('click', () => {
+  const $el = document.querySelector('.destroy').parentNode
+  $el.parentElement.remove()
+})
+
+// data-index
+document.querySelector('.toggle').addEventListener('click', (e) => {
+  const $div = document.querySelector('.toggle').parentNode
+  $div.parentElement.setAttribute('class', 'completed')
+})
