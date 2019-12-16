182d82e37ef4e5f0284783b3fbf450ce7a8252b7
YongHoonJJo <starctak@gmail.com>
2019-12-16 21:53:15 +0900

    [Mission01] todo list의 체크박스를 클릭하여 complete 상태로 변경. (li tag 에 completed class 추가)

--- mission001/YongHoonJJo/js/app.js
+++ mission001/YongHoonJJo/js/app.js
@@ -1,11 +1,13 @@
 function TodoList(selector) {
-  this.todoList = document.querySelector(selector) 
+  this.items = []
+  this.$todoList = document.querySelector(selector) // #todo-list
 }
 
 TodoList.prototype.render = function(items) {
+  console.log(items)
   const itemsHtmlString = items.reduce((acc, item, index) => {
     const { content, completed } = item
-    const inputHtmlString = `<input class="toggle" type="checkbox">`
+    const inputHtmlString = `<input class="toggle" type="checkbox" data-action=check-${index} ${completed && 'checked'}>`
     const labelHtmlString = `<label class="label">${content}</label>`
     const buttonHtmlString = `<button class="destroy"></button>`
     const viewHtmlString = `<div class="view">${inputHtmlString}${labelHtmlString}${buttonHtmlString}</div>`
@@ -13,24 +15,46 @@ TodoList.prototype.render = function(items) {
     return `${acc}${liHtmlString}`
   }, '')
 
-  this.todoList.innerHTML = itemsHtmlString
+  this.$todoList.innerHTML = itemsHtmlString
 }
 
+TodoList.prototype.addState = function(item) {
+  this.items.push(item)
+  this.render(this.items)
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-  const items = []
-  const $input = document.querySelector('.new-todo')
-  const TodoListComponent = new TodoList('#todo-list')
+TodoList.prototype.toggleState = function(idx) {
+  this.items = this.items.map((item, index) => idx == index ? ({...item, completed: !item.completed}) : ({...item}))
+  this.render(this.items) 
+}
 
-  $input.addEventListener('keydown', e => {
+function App(inputSelector, todoListSelector) {
+  this.$input = document.querySelector(inputSelector)
+  this.TodoListComponent = new TodoList(todoListSelector)
+
+  this.$input.addEventListener('keydown', e => {
     if(e.key === 'Enter') {
       const item = {
         content: e.target.value,
         completed: false,
       }
-      items.push(item)
-      TodoListComponent.render(items)
+      this.TodoListComponent.addState(item)
     } 
   })
 
+  this.TodoListComponent.$todoList.addEventListener('click', (e) => {
+    const datasetAction = e.target.dataset.action
+    if(!datasetAction) return ;
+
+    const [action, idx] = datasetAction.split('-')
+    switch(action) {
+      case 'check': this.TodoListComponent.toggleState(idx); break;
+    }
+  })
+}
+
+App.prototype.toggle
+
+document.addEventListener('DOMContentLoaded', () => {
+  new App('.new-todo', '#todo-list')
 })
