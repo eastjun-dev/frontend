6522617c116b701c5761e158060d8deb933b3891
YongHoonJJo <starctak@gmail.com>
2019-12-17 14:09:07 +0900

    [Mission01] todo list의 상태값을 확인하여, 해야할 일과, 완료한 일을 클릭하면 해당 상태의 아이템만 보여주기

--- mission001/YongHoonJJo/js/app.js
+++ mission001/YongHoonJJo/js/app.js
@@ -1,15 +1,29 @@
 function TodoList(selector) {
   this.items = []
+  this.className = 'all'
   this.$todoList = document.querySelector(selector) // #todo-list
+  this.$filters = document.querySelector('ul.filters')
+
+  this.$filters.addEventListener('click', (e) => {
+    if(e.target === this.$filters) return ;
+
+    const aTags = document.querySelectorAll('ul.filters li a')
+    for(const tag of aTags) tag.classList.remove('selected')
+    
+    this.className = e.target.className
+    e.target.classList.add('selected')
+
+    this.renderByFilter(this.className)
+  })
 
   this.$todoList.addEventListener('click', (e) => {
     const datasetAction = e.target.dataset.action
     if(!datasetAction) return ;
 
-    const [action, idx] = datasetAction.split('-')
+    const [action, id] = datasetAction.split('-')
     switch(action) {
-      case 'check': this.toggleState(idx); break;
-      case 'remove': this.removeState(idx); break; 
+      case 'check': this.toggleState(id); break;
+      case 'remove': this.removeState(id); break; 
     }
   })
 
@@ -17,21 +31,39 @@ function TodoList(selector) {
     const datasetAction = e.target.dataset.action
     if(!datasetAction) return ;
 
-    const [action, idx] = datasetAction.split('-')
+    const [action, id] = datasetAction.split('-')
     switch(action) {
-      case 'edit': this.toggleEditView(idx); break;
+      case 'edit': this.toggleEditView(id); break;
     } 
   })
 }
 
+TodoList.prototype.renderByFilter = function(filter) {
+  switch(filter) {
+    case 'active': this.activeRender(this.items); break;
+    case 'completed': this.completedRender(this.items); break;
+    default: this.render(this.items);
+  }
+}
+
+TodoList.prototype.activeRender = function(items) {
+  const activeItems = items.filter((item) => !item.completed)
+  this.render(activeItems) 
+}
+
+TodoList.prototype.completedRender = function(items) {
+  const completedItems = items.filter((item) => item.completed)
+  this.render(completedItems) 
+}
+
 TodoList.prototype.render = function(items) {
-  const itemsHtmlString = items.reduce((acc, item, index) => {
-    const { content, completed } = item
-    const inputHtmlString = `<input class="toggle" type="checkbox" data-action=check-${index} ${completed && 'checked'}>`
-    const labelHtmlString = `<label class="label" data-action=edit-${index}>${content}</label>`
-    const buttonHtmlString = `<button class="destroy" data-action=remove-${index}></button>`
+  const itemsHtmlString = items.reduce((acc, item) => {
+    const { id, content, completed } = item
+    const inputHtmlString = `<input class="toggle" type="checkbox" data-action=check-${id} ${completed && 'checked'}>`
+    const labelHtmlString = `<label class="label" data-action=edit-${id}>${content}</label>`
+    const buttonHtmlString = `<button class="destroy" data-action=remove-${id}></button>`
     const viewHtmlString = `<div class="view">${inputHtmlString}${labelHtmlString}${buttonHtmlString}</div>`
-    const liHtmlString = `<li ${completed && 'class="completed"'} data-action=edit-${index}>${viewHtmlString}<input class="edit" value="${content}"></li>`
+    const liHtmlString = `<li ${completed && 'class="completed"'} data-action=edit-${id}>${viewHtmlString}<input class="edit" value="${content}"></li>`
     return `${acc}${liHtmlString}`
   }, '')
 
@@ -46,21 +78,21 @@ TodoList.prototype.renderCounterContainer = function(items) {
 
 TodoList.prototype.addState = function(item) {
   this.items.push(item)
-  this.render(this.items)
+  this.renderByFilter(this.className)
 }
 
-TodoList.prototype.toggleState = function(idx) {
-  this.items = this.items.map((item, index) => idx == index ? ({...item, completed: !item.completed}) : ({...item}))
-  this.render(this.items) 
+TodoList.prototype.toggleState = function(id) {
+  this.items = this.items.map((item) => id == item.id ? ({...item, completed: !item.completed}) : ({...item}))
+  this.renderByFilter(this.className)
 }
 
-TodoList.prototype.removeState = function(idx) {
-  this.items = this.items.filter((items, index) => idx != index)
-  this.render(this.items) 
+TodoList.prototype.removeState = function(id) {
+  this.items = this.items.filter((item) => id != item.id)
+  this.renderByFilter(this.className)
 }
 
-TodoList.prototype.toggleEditView = function(idx) {
-  const $liElement = document.querySelector(`li[data-action=edit-${idx}]`)
+TodoList.prototype.toggleEditView = function(id) {
+  const $liElement = document.querySelector(`li[data-action=edit-${id}]`)
   const $inputElement = $liElement.querySelector('input.edit')
  
   $liElement.setAttribute('class', 'editing')
@@ -68,23 +100,25 @@ TodoList.prototype.toggleEditView = function(idx) {
   $inputElement.addEventListener('keydown', (e) => {
     switch(e.key) {
       case 'Escape': this.render(this.items); break;
-      case 'Enter': this.editContent(idx, e.target.value); break;
+      case 'Enter': this.editContent(id, e.target.value); break;
     }
   }) 
 }
 
-TodoList.prototype.editContent = function(idx, content) {
-  this.items = this.items.map((item, index) => index == idx ? {...item, content} : {...item})
-  this.render(this.items)
+TodoList.prototype.editContent = function(id, content) {
+  this.items = this.items.map((item) => index.id == id ? {...item, content} : {...item})
+  this.renderByFilter(this.className)
 }
 
 function App(inputSelector, todoListSelector) {
+  this.index = 0
   this.$input = document.querySelector(inputSelector)
   this.TodoListComponent = new TodoList(todoListSelector)
 
   this.$input.addEventListener('keydown', e => {
     if(e.key === 'Enter') {
       const item = {
+        id: this.index++,
         content: e.target.value,
         completed: false,
       }
@@ -93,8 +127,6 @@ function App(inputSelector, todoListSelector) {
     } 
   })
 }
-
-App.prototype.toggle
 
 document.addEventListener('DOMContentLoaded', () => {
   new App('.new-todo', '#todo-list')
