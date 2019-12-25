97ce9979252ce401f08f37f1475076e575b2e2d8
eastjun <eastjun@woowahan.com>
2019-12-25 19:07:27 +0900

    feat(utils): add utils

---
+++ mission002/eastjun/js/utils/utils.js
@@ -0,0 +1,20 @@
+const DEFAULT_USER = 'eastjun'
+
+const errorMessagesMap = {
+  NOT_NEW_INSTANCE: 'new를 이용해 생성한 인스턴스가 아닙니다.',
+  INVALID_DATA: '올바른 형태의 데이터가 아닙니다!',
+  IS_NOT_ARRAY: '올바른 배열 형태의 데이터가 아닙니다.'
+}
+
+const todoItemStatusMap = {
+  ALL: 'all',
+  ACTIVE: 'active',
+  COMPLETED: 'completed'
+}
+
+const keyboardMap = {
+  ENTER: 'Enter',
+  ESC: 'Escape'
+}
+
+export { DEFAULT_USER, errorMessagesMap, todoItemStatusMap, keyboardMap }
