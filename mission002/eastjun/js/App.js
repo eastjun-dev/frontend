1d79551c7e9c7e33e8194c4bcf692ded96ee2290
eastjun <eastjun@woowahan.com>
2019-12-25 19:05:29 +0900

    feat(boilerplate): init boilerplate

---
+++ mission002/eastjun/js/App.js
@@ -0,0 +1,50 @@
+import TodoList from './components/TodoList.js'
+import TodoInput from './components/TodoInput.js'
+import TodoCount from './components/TodoCount.js'
+import TodoStatus from './components/TodoStatus.js'
+import api from './api/api.js'
+
+function TodoApp() {
+    this.todoItems = []
+
+    this.setState = (updatedItems) => {
+        this.todoItems = updatedItems
+        todoList.setState(this.todoItems)
+        todoCount.setState(this.todoItems)
+    }
+
+    new TodoInput({
+        setState: (todoItems) => {
+            this.todoItems = todoItems
+            this.setState(this.todoItems)
+        }
+    })
+
+    new TodoStatus({
+        todoItems: this.todoItems,
+        filter: (filteredItems) => {
+            this.setState(filteredItems)
+        }
+    })
+
+    const todoList = new TodoList({
+        setState: (todoItems) => {
+            this.todoItems = todoItems
+            this.setState(this.todoItems)
+        },
+        onToggleItem: (index) => {
+            this.todoItems[index].isCompleted = !this.todoItems[index].isCompleted
+            this.setState(this.todoItems)
+        },
+        onRemoveItem: (index) => {
+            this.todoItems.splice(index, 1)
+            this.setState(this.todoItems)
+        }
+    })
+
+    const todoCount = new TodoCount({
+        todoItems: this.todoItems
+    })
+}
+
+new TodoApp()
