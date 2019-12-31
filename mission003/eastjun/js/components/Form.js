3ac08b7053ff35dd7629f24b62d6e5235d5aab1e
eastjun <eastjun@woowahan.com>
2020-01-01 03:34:49 +0900

    feat(nextButton): add validation to show next button when fill all form fields

--- mission003/eastjun/js/components/Form.js
+++ mission003/eastjun/js/components/Form.js
@@ -3,27 +3,66 @@ import validator from '../utils/validator.js'
 export default function Form() {
   const $email = document.querySelector('#email')
   const $phone = document.querySelector('#phone')
+  const $name = document.querySelector('#name')
+  const $password = document.querySelector('#password')
+  const $introduce = document.querySelector('#introduce')
+  const $agreement = document.querySelector('#agreement')
 
-  this.name = ''
-  this.email = ''
-  this.phone = ''
-  this.password = ''
-  this.introduce = ''
-  this.agreement = ''
-
+  this.formFields = {
+    name: '',
+    email: '',
+    phone: '',
+    password: '',
+    introduce: '',
+    agreement: ''
+  }
 
   const initEventListener = () => {
+    $name.addEventListener('keyup', (event) => {
+      this.formFields.name = event.target.value
+      isFilledForm()
+    })
+
+    $password.addEventListener('keyup', (event) => {
+      this.formFields.password = event.target.value
+      isFilledForm()
+    })
+
+    $introduce.addEventListener('keyup', (event) => {
+      this.formFields.introduce = event.target.value
+      isFilledForm()
+    })
+
     $email.addEventListener('keyup', (event) => {
-      const value = event.target.value
-      this.email = value
-      togglePrivacyCheckbox(this.email)
+      this.formFields.email = event.target.value
+      togglePrivacyCheckbox(this.formFields.email)
+      isFilledForm()
     })
 
     $phone.addEventListener('keyup', (event) => {
-      const value = event.target.value
-      this.phone = value
-      togglePrivacyCheckbox(this.phone)
+      this.formFields.phone = event.target.value
+      togglePrivacyCheckbox(this.formFields.phone)
+      isFilledForm()
     })
+
+    $agreement.addEventListener('click', (event) => {
+      this.formFields.agreement = event.target.checked
+      isFilledForm()
+    })
+
+  }
+
+  const isFilledForm = () => {
+    if (this.formFields.agreement &&
+        validator.isNotEmptyString(this.formFields.name) &&
+        validator.isNotEmptyString(this.formFields.email) &&
+        validator.isNotEmptyString(this.formFields.phone) &&
+        validator.isNotEmptyString(this.formFields.password) &&
+        validator.isNotEmptyString(this.formFields.introduce)
+    ) {
+      const $step1CompleteButton = document.querySelector('.step1-complete-btn')
+      $step1CompleteButton.classList.remove('hidden')
+    }
   }
 
   const togglePrivacyCheckbox = (value) => {
