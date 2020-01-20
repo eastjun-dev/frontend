13996266ae2d059547cd8861bdcdd3805b628e54
Hosek Na <hsna7024@gmail.com>
2020-01-20 17:04:44 +0900

    feat: Add remove todos
    When 'X' button is clicked, it is removed

--- mission001/hsna7024/js/TodoList.js
+++ mission001/hsna7024/js/TodoList.js
@@ -4,8 +4,14 @@ export default function TodoList(params) {
 
   $target.addEventListener("click", e => {
     if (e.target.className === "toggle") {
-      const { id } = e.toElement.parentElement.parentElement.dataset
-      data[id].isCompleted = e.toElement.checked
+      const { id } = e.toElement.parentElement.parentElement.dataset;
+      data[id].isCompleted = e.toElement.checked;
+      this.render();
+    }
+
+    if (e.target.className === "destroy") {
+      const { id } = e.toElement.parentElement.parentElement.dataset;
+      data.splice(id, 1);
       this.render();
     }
   });
@@ -19,14 +25,13 @@ export default function TodoList(params) {
     $target.innerHTML = data
       .map((todo, index) => {
         return todo.isCompleted
-        ? `<li class="completed" data-id="${index}">
+          ? `<li class="completed" data-id="${index}">
         <div class="view">
          <input class="toggle" type="checkbox" checked="true">
          <label class="label">${todo.content}</label>
          <button class="destroy"></button>
        </div></li>`
-        :
-        `<li data-id="${index}">
+          : `<li data-id="${index}">
         <div class="view">
          <input class="toggle" type="checkbox">
          <label class="label">${todo.content}</label>
