0e6d7327e0cc9c04419eab0de5ffcd56a3e11cd1
Hosek Na <hsna7024@gmail.com>
2020-01-20 15:34:00 +0900

    feat: Create 'TodoList' component

---
+++ mission001/hsna7024/js/TodoList.js
@@ -0,0 +1,24 @@
+export default function TodoList(params) {
+  const $target = params.$target;
+  let data = params.data || [];
+
+  this.setState = nextData => {
+    data = nextData;
+    this.render();
+  };
+
+  this.render = () => {
+    $target.innerHTML = data
+      .map((todo, index) => {
+        return `<li data-id="${index}">
+        <div class="view">
+         <input class="toggle" type="checkbox">
+         <label class="label">${todo.content}</label>
+         <button class="destroy"></button>
+       </div></li>`;
+      })
+      .join("");
+  };
+
+  this.render();
+}
