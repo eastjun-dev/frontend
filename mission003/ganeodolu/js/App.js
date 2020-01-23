fc5e95da01951152d483a348115a46e244bb410f
ganeodolu <ganeodolu@gmail.com>
2020-01-23 22:29:37 +0900

    M3 step2 완료
    data에 입력값 저장

--- mission003/ganeodolu/js/App.js
+++ mission003/ganeodolu/js/App.js
@@ -7,19 +7,25 @@ export default function App(data) {
     this.data = data
 
     const $targetUserForm = document.querySelector('.user-primary-info-container')
-    const $targetAgreement = document.querySelector('.mdc-form-field')
+    const $targetInput = document.querySelectorAll('.mdc-text-field input')
     const $targetPhone = document.getElementById('phone')
     const $targetEmail = document.getElementById('email')
     const $targetIntroduce = document.getElementById('introduce')
-    const $targetInput = document.querySelectorAll('.mdc-text-field input')
+    const $targetAgreement = document.querySelector('.mdc-form-field')
+
     const $targetCompleteButton1 = document.querySelector('.step1-complete-btn')
-    console.log($targetCompleteButton1)
+    const $targetCompleteButton2 = document.querySelector('.step2-complete-btn')
+    const $targetCompleteButton3 = document.querySelector('.step3-complete-btn')
+
+    const $targetTitle = document.getElementById('my-dialog-title')
+
     const $targetStep1 = document.getElementById('step1')
     const $targetStep2 = document.getElementById('step2')
     const $targetStep3 = document.getElementById('step3')
-    console.log($targetStep1)
-    console.log($targetStep2)
-    console.log($targetStep3)
+
+    const $targetAddFamilyFormData = document.querySelector('.user-family-forms-data')
+    const $targetAddFamilyButton = document.getElementById('add-family-button')
+
     const userForm = new UserForm({
         $target: $targetUserForm,
         data: this.data,
@@ -49,20 +55,21 @@ export default function App(data) {
     })
 
     const $targetCheckBox = document.querySelector('.mdc-checkbox')
-    // console.log($targetCheckBox)
-    let agreementCheck
+    let agreementCheck = false
 
     $targetCheckBox.addEventListener('click', (e) => {
         agreementCheck = !agreementCheck
+        console.log(agreementCheck)
     })
 
 
     $targetCompleteButton1.addEventListener('click', (e) => {
+        e.preventDefault()
         for (let i = 0; i < 3; i++) { // 비번제외
             console.log($targetInput[i].name)
             console.log($targetInput[i].value)
             // data.push($targetInput[i].name + ':',$targetInput[i].value)
-            // data[$targetInput[i].name] = $targetInput[i].value
+            data[$targetInput[i].name] = $targetInput[i].value
 
             if ($targetInput[i].value) {
                 // console.log($targetInput[i].name)
@@ -71,39 +78,78 @@ export default function App(data) {
 
             }
         }
-        $targetStep1.classList.add('hidden')
-        $targetStep2.classList.remove('hidden')
-        // this.data['introduce'] = $targetIntroduce.value;
-        // console.log(this.data)
+        if (e.target.classList.contains('mdc-button__ripple') && agreementCheck) {
+            $targetStep1.classList.add('hidden')
+            $targetTitle.textContent = '가족 구성원 추가 2/3'
 
+            $targetStep2.classList.remove('hidden')
+            this.data['introduce'] = $targetIntroduce.value;
+            console.log(this.data)
+        }
 
-        console.log($targetStep1)
-        console.log($targetStep2)
-        console.log($targetStep3)
+    })
+    let renderedHTMLFamily = ``
+    // this.data = {}
+    this.data['Family'] = { name: [], relation: [] }
+    let $targetFamilyContainer = document.querySelectorAll('.flex-container div input')
+    // const $targetFamilyContainer = document.querySelector('.flex-container')
+    // console.log($targetFamilyContainer)
+    $targetAddFamilyButton.addEventListener('click', (e) => {
+        $targetFamilyContainer = document.querySelectorAll('.flex-container div input')
+        const FamilyContainerLength = $targetFamilyContainer.length
+        console.log($targetFamilyContainer)
+        console.log(FamilyContainerLength)
 
+        e.preventDefault()
 
+        if ($targetFamilyContainer[0].value && $targetFamilyContainer[1].value) {
+            renderedHTMLFamily +=
+                `<div data-index class="flex-container user-family-form-fields">
+            <div class="mdc-text-field mrg-right-10" data-mdc-auto-init="MDCTextField">
+                <input type="text" class="mdc-text-field__input" type="text" name="family-name-${FamilyContainerLength / 2}" value=${$targetFamilyContainer[0].value}>
+                <label for="family-name-" class="mdc-floating-label">이름</label>
+                <div class="mdc-line-ripple"></div>
+            </div>
+            <div class="mdc-text-field" data-mdc-auto-init="MDCTextField">
+                <input type="text" class="mdc-text-field__input" type="text" name="family-relations--${FamilyContainerLength / 2}" value=${$targetFamilyContainer[1].value}>
+                <label for="family-relations-" class="mdc-floating-label">관계</label>
+                <div class="mdc-line-ripple"></div>
+            </div>
+            </div>
+            `
+            this.data['Family']['name'].push($targetFamilyContainer[0].value)
+            this.data['Family']['relation'].push($targetFamilyContainer[1].value)
 
+            console.log(this.data)
+            $targetFamilyContainer[0].value = ''
+            $targetFamilyContainer[1].value = ''
+            $targetFamilyContainer[0].focus()
+        }
+        $targetAddFamilyFormData.innerHTML = renderedHTMLFamily
 
+        mdc.autoInit()
 
     })
 
 
-    // data.push({
-    //     name: $targetInput[0].value,
-    //     phone: $targetInput[1].value,
-    //     email: $targetInput[2].value,
-    //     password: $targetInput[3].value,
-    //     introduce: $targetInput[4].value,
-    //     agreement: agreementCheck,
+    $targetCompleteButton2.addEventListener('click', (e) => {
+        e.preventDefault()
+        $targetStep2.classList.add('hidden')
+        console.log($targetTitle)
+        if ($targetFamilyContainer) {
+            console.log($targetFamilyContainer)
+        }
+
+        $targetTitle.textContent = '입력완료 3/3'
+
+        $targetStep3.classList.remove('hidden')
+    })
+
+    $targetCompleteButton3.addEventListener('click', (e) => {
+        e.preventDefault()
+        $targetStep3.classList.add('hidden')
 
 
-    //     }
-
-    // )
-
-
-
-
-
+    })
 
 }
