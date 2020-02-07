bb3a60cf73799e3a1ce5a2530b116c5c2bfed7bd
Hosek Na <hsna7024@gmail.com>
2020-02-07 15:25:48 +0900

    feat: Create api.js

---
+++ mission002/hsna7024/js/utils/api.js
@@ -0,0 +1,33 @@
+const API_URL = "http://todo-api.roto.codes";
+
+const request = async (uri, method) => {
+  try {
+    const res = await fetch(uri, method);
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const api = {
+  getTodos: async username => request(`${API_URL}/${username}`),
+  postTodo: async (username, todoText) => {
+    return request(`${API_URL}/${username}`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ content: todoText }),
+    })
+  },
+  removeTodo: async (username, id) => {
+    return request(`${API_URL}/${username}/${id}`, {
+      method: 'DELETE',
+    })
+  },
+  toggleTodo: async (username, id) => {
+    return request(`${API_URL}/${username}/${id}/toggle`, {
+      method: 'PUT',
+    })
+  },
+}
