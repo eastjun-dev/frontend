7024079061d9efbeb8ba26070a6ca740fb9be5dd
Hosek Na <hsna7024@gmail.com>
2020-02-13 22:36:57 +0900

    style: Update code style

--- mission002/hsna7024/js/TodoList.js
+++ mission002/hsna7024/js/TodoList.js
@@ -2,20 +2,27 @@ import { classNameMap, keyMap, errorMessageMap } from "./utils/constants.js";
 import { todoListTemplate } from "./utils/templates.js";
 
 export default function TodoList(params) {
-  const { $target, toggleTodo, removeTodo, filterTodos, updateTodoByIndex } = params;
+  const {
+    $target,
+    toggleTodo,
+    removeTodo,
+    filterTodos,
+    updateTodoByIndex
+  } = params;
   let todos = params.todos || [];
   let filter = params.filter || "";
   let filteredTodos = [];
 
-  const onEditMode = ($element) => $element.classList.add(classNameMap.EDIT_MODE);
-  const offEditMode = ($element) => $element.classList.remove(classNameMap.EDIT_MODE)
-  const onEnterInEditMode = ($element) => {
+  const onEditMode = $element => $element.classList.add(classNameMap.EDIT_MODE);
+  const offEditMode = $element =>
+    $element.classList.remove(classNameMap.EDIT_MODE);
+  const onEnterInEditMode = $element => {
     const { id } = $element.closest("li").dataset;
     const index = todos.findIndex(todo => todo._id === id);
-    const content = $element.value
+    const content = $element.value;
     updateTodoByIndex(index, content);
     offEditMode($element.closest("li"));
-  }
+  };
 
   if ($target === null) {
     throw new Error(errorMessageMap.IS_NO_TARGET);
@@ -25,8 +32,7 @@ export default function TodoList(params) {
     const { id } = e.target.closest("li").dataset;
     if (e.target.classList.contains(classNameMap.TOGGLE)) {
       toggleTodo(id);
-    }
-    else if (e.target.classList.contains(classNameMap.REMOVE)) {
+    } else if (e.target.classList.contains(classNameMap.REMOVE)) {
       removeTodo(id);
     }
   });
@@ -38,15 +44,13 @@ export default function TodoList(params) {
   });
 
   $target.addEventListener("keydown", e => {
-    if (!(e.target.classList.contains(classNameMap.EDIT))){
+    if (!e.target.classList.contains(classNameMap.EDIT)) {
       return;
-    }
-    else {
+    } else {
       if (e.key === keyMap.ENTER && e.target.value) {
         onEnterInEditMode(e.target);
         this.render();
-      }
-      else if (e.key === keyMap.ESC) {
+      } else if (e.key === keyMap.ESC) {
         offEditMode(e.target.closest("li"));
       }
     }
